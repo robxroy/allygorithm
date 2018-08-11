1c2c7eb44cce277be47ae6c45517771e90703f4b
Roy <rxbrxy@gmail.com>
2018-08-11 19:53:23 -0400

    ally

---
+++ app/data/allies.js
@@ -0,0 +1,30 @@
+
+var allies = [
+	{
+		name: "Malcolm",
+		photo: "https://cdn-az.allevents.in/banners/de3e1452552aa408eae8990574154fa2-rimg-w400-h400-gmir",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Martin",
+		photo: "https://www.biography.com/.image/t_share/MTE5NTU2MzE2MjgwNDg5NDgz/martin-luther-king-jr-9365086-2-402.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "David",
+		photo: "https://www.insidehighered.com/sites/default/server_files/media/Duke_1.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Gloria",
+		photo: "https://pbs.twimg.com/profile_images/442429704511827968/eUr4mETO_400x400.jpeg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "David",
+		photo: "https://i2.wp.com/www.courthousenews.com/wp-content/uploads/2017/01/SheriffClarke.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = allies;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,66 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var express = require("express")
+
+var Alliances = require("../data/allies.js").Alliances;
+var Ally = require("../data/allies.js").Ally;
+
+//var newAlliance = new alliances ("./app/data/allyList.jason")
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/allies", function(req, res) {
+    res.json(allyList.alliances);
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+
+// MUST HAVE ALGORTHIM TO MATCH INTERESTS OF TWO USES
+// IF USERS MATCH EXACTLY, POST OTHER PROFILE
+// IF USERS
+
+
+
+
+  app.post("/api/allies", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    var newAlly = new Ally (req.body.name, req.body.photo, req.body.survey);
+   
+
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    tableData = [];
+    waitListData = [];
+
+    console.log(tableData);
+  });
+};
